c58ed82f65f5f00ac3e18bd2a1d5677acb6178d1
Himanshu Tejan <himanshu.tejan@paytm.com>
2023-08-17 11:49:25 +0530

    added multiply.cpp file

--- multiply.cpp
+++ multiply.cpp
@@ -2,10 +2,10 @@
 using namespace std;
 int main()
 {
-    int a=0,b=0,sum=0;
+    int a=0,b=0,prd=0;
     a=3,b=2;
-    sum=a+b;
-    cout<<"sum is: "<<sum<<endl;
+    prd=a * b;
+    cout<<"prdct is: "<<prd<<endl;
     
     return 0;
 }
